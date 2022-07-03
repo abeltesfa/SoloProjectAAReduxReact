b8f7a351a5ea71a81f245ec107aba068bae6798c
Abel Tesfa <abeltesfa@gmail.com>
2022-07-02 21:22:08 -0400

    create post css done

--- frontend/src/components/AddPost/index.js
+++ frontend/src/components/AddPost/index.js
@@ -48,48 +48,50 @@ const AddPost = () => {
   }
 
   return (
-    <div className='create-post-container'>
-      <div className='inputBox'>
-        <div>
-        <h1>Create Post</h1>
+    <div className='create-post-outer'>
+      <div className='create-post-container'>
+        <div className='inputBox'>
+          <div className='create-post-title'>
+            <h1>Create Post</h1>
+          </div>
+          <div>
+            {hasSubmitted && validationErrors.length > 0 && (
+              <div className='create-post-errors'>
+                The following errors were found:
+                <ul>
+                  {validationErrors.map(error => (
+                    <li key={error}>{error}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+          <form onSubmit={onSubmit}>
+            <div>
+              <input
+                type='text'
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                placeholder='Title'
+                name='title'
+                className='create-post-title-input'
+              />
+            </div>
+            <div>
+              <textarea
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                name='body'
+                placeholder='Add your post'
+                rows='10'
+                className='create-post-body-input'
+              ></textarea>
+            </div>
+            <div>
+              <button className='create-post-submit-btn' type='submit'>Submit</button>
+            </div>
+          </form>
         </div>
-        <div>
-        {hasSubmitted && validationErrors.length > 0 && (
-          <div>
-            The following errors were found:
-            <ul>
-              {validationErrors.map(error => (
-                <li key={error}>{error}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-        </div>
-        <form onSubmit={onSubmit}>
-          <div>
-            <input
-              type='text'
-              onChange={(e) => setTitle(e.target.value)}
-              value={title}
-              placeholder='Title'
-              name='title'
-              className='create-post-title-input'
-            />
-          </div>
-          <div>
-            <textarea
-              value={body}
-              onChange={(e) => setBody(e.target.value)}
-              name='body'
-              placeholder='Add your post'
-              rows='10'
-              className='create-post-body-input'
-            ></textarea>
-          </div>
-          <div>
-            <button className='create-post-submit-btn' type='submit'>Submit</button>
-          </div>
-        </form>
       </div>
     </div>
   );
