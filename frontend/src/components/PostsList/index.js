2e14b3a3f1969b4271b7bca88c551dc512358d9e
Abel Tesfa <abeltesfa@gmail.com>
2022-07-03 04:13:16 -0400

    basic elements fulfilled

--- frontend/src/components/PostsList/index.js
+++ frontend/src/components/PostsList/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
 import { fetchPosts } from '../../store/postReducer';
 import "./PostsList.css"
+import aboutPic from './IMG_0788.JPG'
 
 
 const PostsList = () => {
@@ -11,6 +12,7 @@ const PostsList = () => {
 
     const posts = useSelector(state => state.posts);
     const session = useSelector(state => state.session);
+    const user = useSelector(state => state.session.user);
 
 
 
@@ -26,7 +28,13 @@ const PostsList = () => {
 
     return (
         <div className='body-posts-container'>
-            <div className='body-leftside-container'></div>
+            <div className='body-leftside-container'>
+                <div className='welcome-text'>
+                    <h1>Welcome to extraMedium</h1>
+                    {!user && (<p>(Please sign in to create posts)</p>)}
+                    <h3>extraMedium is a site where users can create posts about their favorite topics. </h3>
+                </div>
+            </div>
             <div className='body-middle-container'>
                 <div className='body-create-post-btn-container'>
                     {session.user &&
@@ -44,7 +52,36 @@ const PostsList = () => {
                     ))}
                 </ul>
             </div>
-            <div className='body-rightside-container'></div>
+            <div className='body-rightside-container'>
+                <div>
+                <h1>About:</h1>
+                </div>
+                <div>
+                    <img className='aboutPic' alt='about' src={aboutPic}></img>
+                </div>
+                <div>
+                    Link to the project <a href='https://github.com/abeltesfa/SoloProjectAAReduxReact'>repo</a>
+                </div>
+
+                <div>
+                    Link to my <a href='https://github.com/abeltesfa'>Github</a>
+                </div>
+                <div>
+                    Link to my <a href='https://www.linkedin.com/in/abel-tesfa-93121866/'>LinkedIn</a>
+                </div>
+                <div>
+                    <h2>Technologies Used:</h2>
+                </div>
+                <div>
+                    <ul>
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>CSS</li>
+                        <li>Express</li>
+                        <li>Sequelize</li>
+                    </ul>
+                </div>
+            </div>
 
         </div>
     )
