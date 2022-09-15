29c6b2399d1206fbf0f58ecbdde8b2ee5dcb0125
Abel Tesfa <abeltesfa@gmail.com>
2022-09-14 23:26:13 -0400

    search bar skeleton

---
+++ frontend/src/components/SearchBar/index.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./SearchBar.css"
+
+const SearchBar = () => {
+    const [posts, setPosts] = useState('');
+    const [postData, setPostData] = useState('');
+    const [dropdown, setDropdown] = useState(false);
+    const [searchInput, setSearchInput] = useState('');
+
+    const fetchPosts = () => {
+        fetch('/api/posts/')
+            .then(response => response.json())
+            .then(data => setPosts(data))
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+
+
+
+    const handleChange = (searchWord) => {
+        if (searchWord) {
+            const searchPostResults = posts.posts.filter(post => post.title.toLowerCase().startsWith(searchWord.toLowerCase()))
+
+            setPostData(searchPostResults);
+            setDropdown(true);
+            setSearchInput(searchWord)
+        } else {
+            setSearchInput(searchWord)
+            setDropdown(false)
+        }
+    }
+
+
+    const clickResult = () => {
+        setDropdown(false);
+        setSearchInput('');
+    }
+
+    return (
+        <div className="search-bar">
+            {dropdown &&
+                <div className="search-dropdown-cancel" onClick={() => clickResult()}></div>
+            }
+            <div className="search-field">
+                <input
+                    placeholder="Search Make, Model, or Year"
+                    onChange={(e) => handleChange(e.target.value)}
+                    value={searchInput}
+                >
+                </input>
+            </div>
+            <div className={dropdown ? "search-results" : "search-none"}>
+                {
+                    dropdown && (postData.length) ? (
+                        <>
+                            {postData &&
+                                postData.map(post => (
+                                    <div key={post.id} className="search-user">
+                                        <Link onClick={() => clickResult()} to={`/posts/${post.id}`} >
+                                            <div className="search-post-info">
+                                                <div className="search-posttitle">
+                                                    {post.title}
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                ))
+                            }
+                        </>
+                    )
+                        :
+                        (<div className={dropdown ? "no-show" : "no-hide"} id="no-results">No results found.</div>)
+                }
+
+            </div>
+
+        </div>
+    )
+}
+export default SearchBar;
